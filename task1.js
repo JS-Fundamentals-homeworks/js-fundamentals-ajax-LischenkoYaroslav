76c926d5a9980bdefb1a7946a25fa6b9cdbdbc11
LischeenkoYaroslav <yaroslav9732@gmail.com>
2025-03-04 02:34:49 +0200

    tasks done

--- task1.js
+++ task1.js
@@ -1,7 +1,21 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const list = document.querySelector(".usersList");
+
+async function getUsers() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await data.json();
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.textContent = user.name;
+    list.appendChild(li);
+  });
+}
+
+getUsers();
--- task2.js
+++ task2.js
@@ -5,3 +5,24 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const getUserButton = document.querySelector("#getUserButton");
+const userNameInput = document.querySelector("#userNameInput");
+const userCity = document.querySelector("#userCity");
+
+const usersUrl = "https://jsonplaceholder.typicode.com/users";
+
+function getUserCity() {
+  fetch(usersUrl)
+    .then((response) => response.json())
+    .then((users) => {
+      const user = users.find((user) => user.name === userNameInput.value);
+      if (user) {
+        userCity.textContent = user.address.city;
+      } else {
+        console.log(user + " User not found");
+      }
+    });
+}
+
+getUserButton.addEventListener("click", getUserCity);
